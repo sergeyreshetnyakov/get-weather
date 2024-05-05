4c187a6467886ba558479495b80703b002b39377
sergeyreshetnyakov <sergey.reshetnyakov@icloud.com>
2024-05-05 11:08:37 +0300

    correct date display

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 interface IForecast {
     temperature: {
@@ -8,16 +8,16 @@ interface IForecast {
     };
     precip: {
         duration: number;
-        sum: number
+        sum: number;
     };
     wind: {
-        direction: number
+        direction: number;
         speed: number;
     };
     sunrise: string;
     sunset: string;
-    date: string;
-    uv: number
+    date: any;
+    uv: number;
 }
 
 const App = () => {
@@ -28,7 +28,7 @@ const App = () => {
             navigator.geolocation.getCurrentPosition((position) =>
                 resolve({
                     latitude: position.coords.latitude.toString(),
-                    longitude: position.coords.longitude.toString()
+                    longitude: position.coords.longitude.toString(),
                 })
             );
         })
@@ -38,30 +38,46 @@ const App = () => {
                         location.latitude +
                         "&longitude=" +
                         location.longitude +
-                        "&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_sum,precipitation_hours,wind_speed_10m_max,wind_direction_10m_dominant&forecast_days=1"
+                        "&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_sum,precipitation_hours,wind_speed_10m_max,wind_direction_10m_dominant&forecast_days=7"
                 )
             )
-            .then((res) => setForecast({
-                temperature: {
-                    max: res.data.daily.temperature_2m_max[0],
-                    min: res.data.daily.temperature_2m_min[0],
-                },
-                precip: {
-                    duration: res.data.daily.precipitation_hours[0],
-                    sum: res.data.daily.precipitation_sum[0],
-                },
-                wind: {
-                    direction: res.data.daily.wind_speed_10m_max[0],
-                    speed: res.data.daily.wind_direction_10m_dominant[0],
-                },
-                sunrise: res.data.daily.sunrise[0],
-                sunset: res.data.daily.sunset[0],
-                date: res.data.daily.time[0],
-                uv: res.data.daily.uv_index_max[0]
-            }))
+            .then((res) =>
+                setForecast({
+                    temperature: {
+                        max: res.data.daily.temperature_2m_max[0],
+                        min: res.data.daily.temperature_2m_min[0],
+                    },
+                    precip: {
+                        duration: res.data.daily.precipitation_hours[0],
+                        sum: res.data.daily.precipitation_sum[0],
+                    },
+                    wind: {
+                        direction: res.data.daily.wind_speed_10m_max[0],
+                        speed: res.data.daily.wind_direction_10m_dominant[0],
+                    },
+                    sunrise: res.data.daily.sunrise[0],
+                    sunset: res.data.daily.sunset[0],
+                    date: new Date(
+                        res.data.daily.time[0].slice(0, 4),
+                        res.data.daily.time[0].slice(6, 7) -1,
+                        res.data.daily.time[0].slice(9, 10)
+                    ),
+                    uv: res.data.daily.uv_index_max[0],
+                })
+            );
     }, []);
 
-    return <div>{forecast?.date}</div>;
+    return (
+        <div>
+            <h1>
+                {forecast?.date.toLocaleString("ru", {
+                    month: "long",
+                    day: "numeric",
+                })}
+            </h1>
+            <h2>{forecast?.date.toLocaleString("ru", { weekday: "long" })}</h2>
+        </div>
+    );
 };
 
 export default App;
